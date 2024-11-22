bc142c2be9de2642fcab55db8ec8f3656e2f1d0d
M0H1K4N <nejer911@gmail.com>
2024-11-22 16:26:29 +0400

    Adding game logic

--- script.js
+++ script.js
@@ -1,10 +1,38 @@
-/*
-console.log(document.querySelector('.message').textContent);
-document.querySelector('.message').textContent = '🎉 Correct Number!';
+'use strict';
 
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 10;
+// const message = document.querySelector('.message').textContent = "👌Correct Number"
 
-document.querySelector('.guess').value = 23;
-console.log(document.querySelector('.guess').value);
-*/
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 1121;
+// console.log(document.querySelector('.guess').value)
+const secretNumber = Math.floor(Math.random() * 20) + 1;
+let score = 20;
+document.querySelector('.number').textContent = secretNumber;
+
+console.log(secretNumber);
+
+const checkBtn = document.querySelector('.check');
+
+checkBtn.addEventListener('click', function () {
+  const guessInput = Number(document.querySelector('.guess').value);
+  // console.log(typeof guessInput);
+
+  if (!guessInput) {
+    document.querySelector('.message').textContent =
+      '😒There is no guess number in the input field';
+  } else if (guessInput == secretNumber) {
+    document.querySelector('.message').textContent = '🥇Bingooo';
+    score;
+    document.querySelector('.score').textContent = score;
+  } else if (guessInput > secretNumber) {
+    document.querySelector('.message').textContent = '☝️Number is so HIGH';
+    score--;
+    document.querySelector('.score').textContent = score;
+  } else if (guessInput < secretNumber) {
+    document.querySelector('.message').textContent = '👇Number is so LOW';
+    score--;
+    document.querySelector('.score').textContent = score;
+  } else {
+    document.querySelector('.message').textContent = ' 😊Incorrect number!';
+  }
+});

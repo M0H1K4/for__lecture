c8406d0df88f32c584d606263dfb7f02ba73fb85
M0H1K4N <nejer911@gmail.com>
2024-11-26 19:33:24 +0400

    Finished �

--- script.js
+++ script.js
@@ -1,22 +1,15 @@
 'use strict';
 
-// const message = document.querySelector('.message').textContent = "👌Correct Number"
-
-// document.querySelector('.number').textContent = 13;
-// document.querySelector('.score').textContent = 1121;
-// console.log(document.querySelector('.guess').value)
-//////////////////////////////////////
 let secretNumber = Math.floor(Math.random() * 20) + 1;
 const reLoadBtn = document.querySelector('.again');
 let score = 20;
-let highScore =  0;
+let highScore = 0;
 
 const reloadFunction = () => {
-  // window.location.reload();
   score = 20;
   secretNumber = Math.floor(Math.random() * 20) + 1;
 
-  document.querySelector('.message').textContent = 'Start guessing...';
+ displayMessage('Start guessing...');
   document.querySelector('.score').textContent = score;
   document.querySelector('.number').textContent = '?';
   document.querySelector('.guess').value = '';
@@ -25,17 +18,18 @@ const reloadFunction = () => {
 };
 reLoadBtn.addEventListener('click', reloadFunction);
 
-// console.log(secretNumber);
-
 const checkBtn = document.querySelector('.check');
+
+const displayMessage = message => {
+  document.querySelector('.message').textContent = message;
+};
 
 checkBtn.addEventListener('click', function () {
   const guessInput = Number(document.querySelector('.guess').value);
   // console.log(typeof guessInput);
   // როცა არ არის ინფუთში მონაცემი
   if (!guessInput) {
-    document.querySelector('.message').textContent =
-      '😒There is no guess number in the input field';
+    displayMessage('😒There is no guess number in the input field');
     // როცა მოთამაშე იგებს
   } else if (guessInput == secretNumber) {
     // აქ უნდა შეიცვალოს ბექგრაუნდი
@@ -45,36 +39,44 @@ checkBtn.addEventListener('click', function () {
     document.querySelector('.number');
     document.querySelector('.number').style.width = '30rem';
     //// highScore
-    if(score > highScore ){
+    if (score > highScore) {
       highScore = score;
       document.querySelector('.highscore').textContent = highScore;
     }
-
-    // score;
-    // document.querySelector('.score').textContent = score;
     // როცა ინფუთში ჩაწერილი რიცხვი მეტია ვიდრე გამოსაცნობი რიცხვი
-  } else if (guessInput > secretNumber) {
+  } else if (guessInput !== secretNumber) {
     if (score > 1) {
-      document.querySelector('.message').textContent = '☝️Number is so HIGH';
+      displayMessage(
+        guessInput > secretNumber ? '☝️Number is so HIGH' : '👇Number is so LOW'
+      );
       score--;
       document.querySelector('.score').textContent = score;
     } else {
-      document.querySelector('.message').textContent = ' 💣You LOST!';
-      document.querySelector('.score').textContent = 0;
-    }
-    // როცა ინფუთში ჩაწერილი რიცხვი ნაკლები  გამოსაცნობ რიცხვზე
-  } else if (guessInput < secretNumber) {
-    if (score > 1) {
-      document.querySelector('.message').textContent = '👇Number is so LOW';
-      document.querySelector('.score').textContent = score;
-      score--;
-    } else {
-      document.querySelector('.message').textContent = ' 💣You LOST!';
+      displayMessage(' 💣You LOST!');
       document.querySelector('.score').textContent = 0;
     }
   }
 });
 
+// Deleted Part ^_~
 
-
-
+// else if (guessInput > secretNumber) {
+//   if (score > 1) {
+//     document.querySelector('.message').textContent = '☝️Number is so HIGH';
+//     score--;
+//     document.querySelector('.score').textContent = score;
+//   } else {
+//     document.querySelector('.message').textContent = ' 💣You LOST!';
+//     document.querySelector('.score').textContent = 0;
+//   }
+//   // როცა ინფუთში ჩაწერილი რიცხვი ნაკლები  გამოსაცნობ რიცხვზე
+// } else if (guessInput < secretNumber) {
+//   if (score > 1) {
+//     document.querySelector('.message').textContent = '👇Number is so LOW';
+//     document.querySelector('.score').textContent = score;
+//     score--;
+//   } else {
+//     document.querySelector('.message').textContent = ' 💣You LOST!';
+//     document.querySelector('.score').textContent = 0;
+//   }
+// }
